a96657c1e8f07070baa150003f452dc1c11db56f
x1-carbon <bougienath@gmail.com>
2025-08-12 23:11:36 -0400

    Updated hyptland

---
+++ .config/hypr/script/idleInhibitator.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+SERVICE_NAME=hypridle.service
+
+logger "Starting idle Inhibitor script"
+
+if systemctl --user is-active --quiet "$SERVICE_NAME"; then
+    logger "Stopping hypridle"
+    notify-send "Stoped hypridle"
+    systemctl --user stop "$SERVICE_NAME"
+else
+    logger "Started hypridle"
+    notify-send "Started hypridle"
+    systemctl --user start "$SERVICE_NAME"
+fi
