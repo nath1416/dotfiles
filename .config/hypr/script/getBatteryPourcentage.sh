df8608e275322bd4994ccf2382da87c487751059
x1-carbon <bougienath@gmail.com>
2025-11-16 23:06:01 -0500

    Better battery indicator

--- .config/hypr/script/getBatteryPourcentage.sh
+++ .config/hypr/script/getBatteryPourcentage.sh
@@ -1,7 +1,47 @@
-#! /usr/bin/env sh
+#! /usr/bin/env bash
 
 set -e 
-pourcentage=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep perce | awk '{print $2}')
 
-echo "$pourcentage 󰁹"
+upowerResult=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0)
 
+
+state=$(echo "$upowerResult" | grep state | awk '{print $2}')
+
+state_icon=""
+
+if [[ "$state" == "pending-charge" ]]; then
+    state_icon=" "
+fi
+
+if [[ "$state" == "charging" ]]; then
+    state_icon="󱐋"
+fi
+
+if [[ "$state" == "discharging" ]]; then
+    state_icon="󰶹"
+fi
+
+pourcentage=$(echo "$upowerResult" | grep perce | awk '{print $2}')
+number=${pourcentage%\%}
+
+battery_icon="󰂃"
+
+if   (( number <= 10 )); then
+    battery_icon="󰁺"   # 0–10%
+elif (( number <= 20 )); then
+    battery_icon="󰁻"   # 11–20%
+elif (( number <= 30 )); then
+    battery_icon="󰁼"   # 21–30%
+elif (( number <= 40 )); then
+    battery_icon="󰁽"   # 31–40%
+elif (( number <= 50 )); then
+    battery_icon="󰁿"   # 41–50%
+elif (( number <= 60 )); then
+    battery_icon="󰂀"   # 51–60%
+elif (( number <= 70 )); then
+    battery_icon="󰂁"   # 61–70%
+elif (( number <= 80 )); then
+    battery_icon="󰁹"   # 71–80%
+fi
+
+echo "$pourcentage $battery_icon $state_icon"
